8bf43ab894b042195918f6cfa9908e398a11c80d
Nitin-Anil-Narang <narangnitin520@gmail.com>
2025-07-21 18:50:28 +0530

    Cors fix

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,10 @@ const port = process.env.PORT;
 
 const frontend_url = process.env.FRONTEND_URL;
 //
-// app.use(cors({
-//   origin: frontend_url,
-//   credentials: false
-// }));
+app.use(cors({
+  origin: frontend_url,
+  credentials: true
+}));
 app.use(express.json());
 
 // Below api was used to initally test the DB connect and fetch data (Ignore)
